899fa0e9b49b9106f924d4269caeafe9b8afdbe4
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-06-18 10:14:31 -0600

    added addPetWithOwnerAndImage mutation

--- db/api/mutation_helpers.js
+++ db/api/mutation_helpers.js
@@ -1,5 +1,15 @@
 const knex = require('../knex');
 
 const addPet = (petObject) => knex('pets').insert(petObject).returning('*');
+const addImage = (image_url) => knex('images').insert({ image_url }).returning('*');
+const addOwner = (pet_id, owner_id) => knex('pet_owners').insert({ pet_id, owner_id });
+const addFollower = (pet_id, follower_id) => knex('pet_followers').insert({ pet_id, follower_id });
+const addPetImage = (pet_id, image_id) => knex('pet_images').insert({ pet_id, image_id });
 
-module.exports = { addPet };
+module.exports = {
+  addPet,
+  addImage,
+  addOwner,
+  addFollower,
+  addPetImage
+ };
--- db/types/mutations.js
+++ db/types/mutations.js
@@ -10,8 +10,7 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLFloat,
-  GraphQLNonNull,
-  GraphQLList
+  GraphQLNonNull
 } = graphql;
 
 const mutations = new GraphQLObjectType({
@@ -28,6 +27,33 @@ const mutations = new GraphQLObjectType({
       resolve(parentValue, args) {
         return db.addPet(args)
         .then(newPet => newPet[0]);
+      }
+    },
+    addPetWithOwnerAndImage: {
+      type: PetType,
+      args: {
+        owner_id: { type: new GraphQLNonNull(GraphQLInt) },
+        profile_image_url: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        species: { type: GraphQLString },
+        breed: { type: GraphQLString },
+        age: { type: GraphQLFloat }
+      },
+      resolve(parentValue, { owner_id, profile_image_url, name, species, breed, age }) {
+        return db.addImage(profile_image_url)
+        .then(newImage => {
+          return db.addPet({ name, species, breed, age, profile_image_id: newImage[0].id })
+          .then(newPet => {
+            return Promise.all([
+              db.addOwner(newPet[0].id, owner_id),
+              db.addFollower(newPet[0].id, owner_id),
+              db.addPetImage(newPet[0].id, newImage[0].id)
+            ])
+            .then(() => {
+              return newPet[0];
+            });
+          });
+        });
       }
     }
   })
