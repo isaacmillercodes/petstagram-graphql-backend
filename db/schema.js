811430c86f5f4847f3592f0ebab3bf6195fac996
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-06-21 07:39:20 -0600

    refactored RootMutation

--- db/schema.js
+++ db/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 const { GraphQLSchema } = graphql;
 const RootQuery = require('./types/root_query_type');
-const RootMutation = require('./types/mutations');
+const RootMutation = require('./types/root_mutation_type');
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
